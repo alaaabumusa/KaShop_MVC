d29eebe1e48dc76d0e575306f270277cefe356b9
alaaabumusa <aalaabumussa@gmail.com>
2026-01-23 22:16:30 +0200

    Init project

---
+++ KaShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KaShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=.;Database=mvc_13;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
